5dcd4e4d38b053ac7678c10374db9c83055b7190
Steven Wilson <stevenwilson500@gmail.com>
2016-07-05 01:05:15 -0400

    some cleanup, removal of box, doesn't make sense to be in this library

---
+++ simplevector.py
@@ -0,0 +1,223 @@
+from math import sqrt, sin, cos, radians, atan2, degrees, isclose
+
+
+class Vector2D:
+    'Simple 2D vector type that does what you expect (mostly)'
+
+    __slots__ = ['x', 'y']
+
+    def __init__(self, x, y):
+        'Most efficient way to make a vector: from x and y'
+        self.x = x
+        self.y = y
+
+    @classmethod
+    def from_degrees_and_length(cls, angle, length):
+        'Creates a new vector from an angle in degress and a length'
+        angle = radians(angle)
+        x = cos(angle) * length
+        y = sin(angle) * length
+        return cls(x, y)
+
+    @classmethod
+    def from_radians_and_length(cls, angle, length):
+        'Creates a new vector from an angle in radians and a length'
+        x = cos(angle) * length
+        y = sin(angle) * length
+        return cls(x, y)
+
+    def __repr__(self):
+        return "Vector2D({:.3f}, {:.3f})".format(self.x, self.y)
+
+    def __bytes__(self):
+        raise NotImplementedError("TODO")
+
+    def __add__(self, other):
+        return Vector2D(self.x + other.x,
+                        self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector2D(self.x - other.x,
+                        self.y - other.y)
+
+    def __mul__(self, other):
+        return Vector2D(self.x * other,
+                        self.y * other)
+
+    def __truediv__(self, other):
+        return Vector2D(self.x / other,
+                        self.y / other)
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __isub__(self, other):
+        self.x -= other.x
+        self.y -= other.y
+        return self
+
+    def __imul__(self, other):
+        self.x *= other
+        self.y *= other
+        return self
+
+    def __itruediv__(self, other):
+        self.x /= other
+        self.y /= other
+        return self
+
+    def __neg__(self):
+        return Vector2D(-self.x, -self.y)
+
+    def __pos__(self):
+        return Vector2D(+self.x, +self.y)
+
+    def __abs__(self):
+        return Vector2D(abs(self.x), abs(self.y))
+
+    def __eq__(self, other):
+        return isclose((self - other).length, 0,
+                       rel_tol=0.0001, abs_tol=0.00001)
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __hash__(self):
+        'TODO: Improve hashing function'
+        return hash(hash(self.x) + hash(self.y))
+
+    def __bool__(self):
+        'Any truth-checks against vectors will always pass'
+        return True
+
+    def __getitem__(self, key):
+        '''Allows vec[0] to provide the x component
+            and vec[1] to provide the y component.
+            this will be slower than using obj.x or obj.y'''
+        if key == 0:
+            return self.x
+        elif key == 1:
+            return self.y
+        else:
+            message = "Cannot get the {}th item".format(key)
+            raise IndexError("2D Vectors only have 2 components, " + message)
+
+    def __setitem__(self, key, value):
+        '''Allows vec[0] = x to set the x component
+            and vec[1] = y to set the y component'''
+        if key == 0:
+            self.x = value
+        elif key == 1:
+            self.y = value
+        else:
+            message = "Cannot set the {}th item".format(key)
+            raise IndexError("2D Vectors only have 2 components, " + message)
+
+    def __delitem__(self, key):
+        raise NotImplementedError("Cannot remove a component from a Vector2D")
+
+    def __iter__(self):
+        yield self.x
+        yield self.y
+
+    def __reversed__(self):
+        yield self.y
+        yield self.x
+
+    def __contains__(self, item):
+        'Returns true if either component matches the item'
+        return isclose(item, self.x, rel_tol=0.0001, abs_tol=0.00001) or \
+            isclose(item, self.y, rel_tol=0.0001, abs_tol=0.00001)
+
+    @property
+    def length(self):
+        'The magnitude of the vector'
+        x = self.x
+        y = self.y
+        return sqrt(x * x + y * y)
+
+    @length.setter
+    def length(self, value):
+        'Sets the magnitude of the vector'
+        current = self.length
+        self *= (value / current)
+
+    @property
+    def length_squared(self):
+        'The squared magnitude of the vector.  Fast for length compares'
+        x = self.x
+        y = self.y
+        return x * x + y * y
+
+    def normalize(self):
+        'Grows or Shrinks the vector to a size of 1.0 but maintains angle'
+        self.length = 1
+
+    @property
+    def normalized(self):
+        'Returns a normalized copy of the vector'
+        return self / self.length
+
+    @normalized.setter
+    def normalized(self, value):
+        '''Ensures the vector, when normalized, will match the normalized
+            version of the vector povided'''
+        self.radians = value.radians
+
+    @property
+    def degrees(self):
+        'Returns the angle of the vector in degrees'
+        return degrees(self.radians)
+
+    @degrees.setter
+    def degrees(self, value):
+        'Sets the angle of the vector in degrees'
+        self.radians = radians(value)
+
+    @property
+    def radians(self):
+        'Returns the angle of the vector in radians'
+        return atan2(self.y, self.x)
+
+    @radians.setter
+    def radians(self, value):
+        'Sets the angle of the vector in radians'
+        new_v = Vector2D.from_radians_and_length(value, self.length)
+        self.x = new_v.x
+        self.y = new_v.y
+
+    @property
+    def copy(self):
+        'Makes a copy of the vector'
+        return Vector2D(self.x, self.y)
+
+    def dot_product(self, other):
+        'Returns the dot product'
+        return self.x * other.x + self.y * other.y
+
+    @property
+    def x_vector(self):
+        'Returns a vector with only the x component'
+        return Vector2D(self.x, 0)
+
+    @property
+    def y_vector(self):
+        'Returns a vector with only the y component'
+        return Vector2D(0, self.y)
+
+    @property
+    def mirror(self):
+        'A copy of the vector mirrored over x and y. same as * -1 or -obj'
+        return -self
+
+    @property
+    def mirror_x(self):
+        'A copy of the vector mirrored over the x axis'
+        return Vector2D(self.x, -self.y)
+
+    @property
+    def mirror_y(self):
+        'A copy of the vector mirrored over the y axis'
+        return Vector2D(-self.x, self.y)
---
+++ test_simplevector.py
@@ -0,0 +1,233 @@
+import pytest
+from simplevector import Vector2D
+from random import random
+from math import isclose
+
+pi = 3.14159265358979323846264
+
+
+def rng(a, b):
+    return random() * (b - a) - (b - a) / 2
+
+
+def close_enough(a, b):
+    return isclose(a, b, rel_tol=0.0001, abs_tol=0.00001)
+
+
+def random_vector():
+    return Vector2D(rng(-20, 20),
+                    rng(-20, 20))
+
+
+@pytest.fixture
+def v1():
+    return random_vector()
+
+
+@pytest.fixture
+def v2():
+    return random_vector()
+
+
+def test_concat_length_should_be_less_than_path(v1, v2):
+    v3 = v1 + v2
+    assert (v1.length + v2.length) - v3.length >= 0
+
+
+def test_vector_subtracted_from_itself_should_be_0(v1):
+    v2 = v1 - v1
+    assert close_enough(v2.length, 0)
+
+
+def test_vector_rotated_180_and_negated_should_be_same(v1):
+    v2 = v1.copy
+    v2.degrees += 180
+    v3 = v1 + v2
+    assert close_enough(v3.length, 0)
+
+    v4 = v1.copy
+    v4.radians += pi
+    v5 = v1 + v4
+    assert close_enough(v5.length, 0)
+
+
+def test_vector_divide_by_2_should_half_length(v1):
+    v2 = v1 / 2
+    v3 = v1.copy
+    assert v1 == v3
+    v3 /= 2
+    assert v2 == v3
+    assert close_enough((v1.length / 2) - v3.length, 0)
+
+
+def test_vector_mul_then_div_should_match(v1):
+    v2 = v1 * 3.238 / 3.238
+    assert v1 == v2
+
+
+def test_normalize(v1):
+    v2 = v1.copy
+    v2.normalize()
+    v3 = v1.normalized
+    assert v2 == v3
+    assert v1 != v2
+    assert v1 != v3
+    assert close_enough(v1.radians - v2.radians, 0)
+    assert close_enough(v1.radians - v3.radians, 0)
+    assert close_enough(v1.degrees - v2.degrees, 0)
+    assert close_enough(v1.degrees - v3.degrees, 0)
+    v4 = v1.copy
+    v4.normalized = Vector2D(1, 0)
+    assert close_enough(v4.length, v1.length)
+    assert close_enough(v4.degrees, 0)
+
+
+def test_alternate_constructors(v1):
+    v2 = Vector2D.from_radians_and_length(v1.radians, v1.length)
+    v3 = Vector2D.from_degrees_and_length(v1.degrees, v1.length)
+    assert v1 == v2
+    assert v1 == v3
+
+
+def test_representation():
+    v = Vector2D(3.5, 2.125)
+    assert repr(v) == "Vector2D(3.500, 2.125)"
+
+
+def test_bytes():
+    v = Vector2D(1, 1)
+    with pytest.raises(NotImplementedError):
+        t = bytes(v)
+        print(t)
+
+
+def test_copy(v1):
+    v2 = v1.copy
+    assert v1 == v2
+
+
+def test_index_access(v1, v2):
+    assert close_enough(v1.x - v1[0], 0)
+    assert close_enough(v1.y - v1[1], 0)
+
+    v1[0] = v2.x
+    v1[1] = v2.y
+
+    assert v1 == v2
+
+    with pytest.raises(IndexError):
+        v1.x = v1[2]
+
+    with pytest.raises(IndexError):
+        v1[2] = 5
+
+    with pytest.raises(NotImplementedError):
+        del v1[0]
+
+
+def test_add(v1, v2):
+    v3 = v1 + v2
+    v4 = v1.copy
+    v4 += v2
+    assert v3 == v4
+    assert close_enough(v3.x, v1.x + v2.x)
+    assert close_enough(v3.y, v1.y + v2.y)
+
+
+def test_sub(v1, v2):
+    v3 = v1 - v2
+    v4 = v1.copy
+    v4 -= v2
+    assert v3 == v4
+    assert close_enough(v3.x, v1.x - v2.x)
+    assert close_enough(v3.y, v1.y - v2.y)
+
+
+def test_mul(v1):
+    f = rng(-20, 20)
+    v2 = v1 * f
+    v3 = v1.copy
+    v3 *= f
+    assert v2 == v3
+    assert close_enough(v2.x, v1.x * f)
+    assert close_enough(v2.y, v1.y * f)
+
+
+def test_div(v1):
+    f = rng(-20, 20)
+    v2 = v1 / f
+    v3 = v1.copy
+    v3 /= f
+    assert v2 == v3
+    assert close_enough(v2.x, v1.x / f)
+    assert close_enough(v2.y, v1.y / f)
+
+
+def test_neg(v1):
+    v2 = -v1
+    assert close_enough(v2.length, v1.length)
+    assert close_enough(v1.x, -v2.x)
+    assert close_enough(v1.y, -v2.y)
+
+
+def test_pos(v1):
+    v2 = +v1
+    assert v1 == v2
+
+
+def test_abs(v1):
+    v2 = abs(v1)
+    assert close_enough(abs(v1.x), v2.x)
+    assert close_enough(abs(v1.y), v2.y)
+
+
+def test_hash(v1):
+    ' Collisions should be exeedingly rare '
+    assert hash(v1) == hash(v1)
+    collisions = 0
+    for _ in range(1000):
+        v2 = random_vector()
+        if hash(v1) == hash(v2):
+            collisions += 1
+    assert collisions < 2
+
+
+def test_bool(v1):
+    assert v1
+
+
+def test_sequence_forward(v1):
+    forward = [v1.x, v1.y]
+    for a, b in zip(forward, v1):
+        assert close_enough(a, b)
+
+
+def test_sequence_backward(v1):
+    backward = [v1.y, v1.x]
+    for a, b in zip(backward, reversed(v1)):
+        assert close_enough(a, b)
+
+
+def test_contains(v1):
+    assert v1.x in v1
+    assert v1.y in v1
+
+
+def test_not_contains(v1):
+    v1.x = 0
+    v1.y = 2
+    assert 1 not in v1
+
+
+def test_length(v1):
+    assert close_enough(v1.length_squared,
+                        v1.length * v1.length)
+    v1.length = 10
+    v1.radians = 0
+    assert close_enough(v1.x, 10)
+    assert close_enough(v1.y, 0)
+    assert close_enough(v1.length_squared, 100)
+
+
+def test_dot(v1):
+    assert close_enough(v1.dot_product(v1), v1.length_squared)
